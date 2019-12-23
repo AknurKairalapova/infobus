399cd74e19579a24ddcc84e7ed784736eaceaf27
beingaknur <kairalapova.aknur10@gmail.com>
2019-12-23 11:06:01 +0600

    project init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import { YMaps } from 'react-yandex-maps';
+import MapContainer from './components/Map';
+import Sidebar from './components/Sidebar';
 import './App.css';
+import './scss/main.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <YMaps>
+    <div className="App infobus">
+      <MapContainer />
+
     </div>
+    </YMaps>
+    
   );
 }
 
