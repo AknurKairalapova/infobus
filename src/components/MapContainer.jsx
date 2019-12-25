0bc17ac808f4b19d136a1c9aefce72aaa9127c27
AknurKairalapova <kairalapova.aknur10@gmail.com>
2019-12-25 19:02:02 +0600

    test route added

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -1,87 +1,95 @@
 import React, { Component } from 'react';
 import { Map } from 'react-yandex-maps';
 import axios from 'axios';
-import {DEFAULT_CITY, BASE_API_URL} from '../constants';
+import { DEFAULT_CITY, BASE_API_URL } from '../constants';
 
 class MapContainer extends Component {
-  constructor(props){
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.state = {
-      ymaps: null,
-      isLoadingPoints: false,
-      points: [],
-      error: null
-    };
+		this.state = {
+			ymaps: null,
+			isLoadingPoints: false,
+			points: [],
+			error: null,
+		};
 
-    this.setYMaps = this.setYMaps.bind(this);
-    this.fetchData = this.fetchData.bind(this);
-    this.showRoute = this.showRoute.bind(this);
-  }
+		this.setYMaps = this.setYMaps.bind(this);
+		this.fetchData = this.fetchData.bind(this);
+		this.showRoute = this.showRoute.bind(this);
+	}
 
-  componentDidMount() {
-    const url = BASE_API_URL + DEFAULT_CITY.id + '/routes/56/busses';
-    // this.fetchData(url);
-  }
+	componentDidMount() {
+		const url = BASE_API_URL + DEFAULT_CITY.id + '/routes/56/busses';
+		// this.fetchData(url);
+	}
 
-  setYMaps(ymaps){
-    this.setState({ ymaps});
-  }
+	setYMaps(ymaps) {
+		this.setState({ ymaps });
+	}
 
-  showRoute(){
-    if (this.state.ymaps) {
-      const mr = this.state.ymaps.multiRouter.MultiRoute;
-      const multiRoute = new mr (
-        {
-          // Описание опорных точек мультимаршрута.
-          referencePoints: [
-            [[55.749, 37.524]], 
-            "Москва, Успенский пер. 7"
+	showRoute(ref) {
+		if (this.state.ymaps) {
+			const mr = this.state.ymaps.multiRouter.MultiRoute;
+			const multiRoute = new mr(
+				{
+					// Описание опорных точек мультимаршрута.
+					referencePoints: [
+            [55.749, 37.524], 
+            'Москва, Успенский пер. 7'
           ],
-          // Параметры маршрутизации.
-          params: {
-            routingMode: 'masstransit'
-          }
-        },
-        {
-          // Автоматически устанавливать границы карты так, чтобы маршрут был виден целиком.
-          boundsAutoApply: true,
+					// Параметры маршрутизации.
+					params: {
+						routingMode: 'masstransit',
+					},
+				},
+				{
+					// Автоматически устанавливать границы карты так, чтобы маршрут был виден целиком.
+					boundsAutoApply: true,
           // Внешний вид линии маршрута.
-          // routeActiveStrokeWidth: 6,
-          // routeActiveStrokeColor: "#fa6600"
-        }
-      );
+          viaPointIconRadius: 7,
+        viaPointIconFillColor: "#000088",
+        viaPointActiveIconFillColor: "#E63E92",
+        
+          routeStrokeWidth: 6,
+          routeStrokeColor: "#000",
+					routeActiveStrokeWidth: 6,
+					routeActiveStrokeColor: "#000"
+				}
+			);
+			console.log('multiRoute ', multiRoute);
+			ref.geoObjects.add(multiRoute);
+			console.log('ref ', ref);
+		}
+	}
 
-      this.map.geoObjects.add(multiRoute);
-    }
-  }
+	fetchData(url) {
+		this.setState({ isLoadingPoints: true });
 
-  fetchData(url){
-    this.setState({	isLoadingPoints: true	});
+		axios
+			.get(url)
+			.then(result => {
+				this.setState({ points: result, isLoadingPoints: false });
+			})
+			.catch(error => {
+				this.setState({ error, isLoadingPoints: false });
+			});
+	}
 
-    axios.get(url)
-      .then(result => {
-        this.setState({points: result, isLoadingPoints: false});
-      })
-      .catch(error => {
-        this.setState({error, isLoadingPoints: false});
-      });
-  }
-
-  render() {
-    return (
-      <div className="infobus__map-container">
-        <Map 
-          defaultState={{ center: [55.739625, 37.54120], zoom: 12 }}
-          modules={["multiRouter.MultiRoute"]}
-          className="infobus__map" 
-          onLoad={ymaps => this.setYMaps(ymaps)}
-          instanceRef={ref => ref && this.showRoute(ref)}
-        />
-        <button onClick={this.showRoute}>Show route</button>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="infobus__map-container">
+				<Map
+					defaultState={{ center: [55.739625, 37.5412], zoom: 12 }}
+					modules={['multiRouter.MultiRoute']}
+					className="infobus__map"
+					onLoad={ymaps => this.setYMaps(ymaps)}
+					instanceRef={ref => ref && this.showRoute(ref)}
+				/>
+				<button onClick={this.showRoute}>Show route</button>
+			</div>
+		);
+	}
 }
 
 export default MapContainer;
--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
-export const DEFAULT_CITY = {id: 1, name: 'Нур-Султан'};
+export const DEFAULT_CITY = {id: 1, name: 'Нур-Султан', location: '71.430411 51.128207'};
 export const BASE_API_URL = 'http://infobus.kz/cities/';
 //'http://infobus.kz/cities/1/routes/56/busses';
